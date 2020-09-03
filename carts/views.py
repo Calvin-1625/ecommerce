712c5b21ee584898c55c8bf6201db37ec4477a1d
calvin-1625 <calvin.pontanilla@gmail.com>
2020-09-03 21:42:57 +0800

    Reuse Address

--- carts/views.py
+++ carts/views.py
@@ -48,7 +48,12 @@ def checkout_home(request):
 
 	billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
 
+	address_qs = None
 	if billing_profile is not None:
+		if request.user.is_authenticated:
+			address_qs = Address.objects.filter(billing_profile=billing_profile)
+		# shipping_address_qs = address_qs.filter(addrees_type='shipping')
+		# billing_address_qs = address_qs.filter(addrees_type='billing')
 		order_obj, order_obj_created = Order.objects.new_or_get(billing_profile, cart_obj)
 		if shipping_address_id:
 			order_obj.shipping_address = Address.objects.get(id=shipping_address_id)
@@ -74,5 +79,6 @@ def checkout_home(request):
 		"login_form": login_form,
 		"guest_form": guest_form,
 		"address_form":address_form,
+		"address_qs": address_qs,
 	}
 	return render(request, "carts/checkout.html", context)
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -22,7 +22,7 @@ from django.contrib.auth.views import LogoutView
 from django.views.generic import TemplateView
 
 
-from addresses.views import checkout_address_create_view
+from addresses.views import checkout_address_create_view, checkout_address_reuse_view
 from accounts.views import login_page, register_page, guest_register_view
 from . import views
 
@@ -34,6 +34,7 @@ urlpatterns = [
     path('contact/',views.contact, name = 'contact'),
     path('login/',login_page, name = 'login'),
     path('checkout/address/create/',checkout_address_create_view, name = 'checkout_address_create'),
+    path('checkout/address/reuse/',checkout_address_reuse_view, name = 'checkout_address_reuse'),
     path('register/guest',guest_register_view, name = 'guest_register'),
     path('logout/',LogoutView.as_view(), name = 'logout'),
     path('register/',register_page, name = 'register'),
