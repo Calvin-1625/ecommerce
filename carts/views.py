e0763a95a87ce1c23e2a15dc61563ed11a5d4bfa
calvin-1625 <calvin.pontanilla@gmail.com>
2020-09-09 21:49:14 +0800

    Handle Ajax

--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 
 from accounts.forms import LoginForm, GuestForm
@@ -16,8 +17,7 @@ def cart_home(request):
 
 def cart_update(request):
 	product_id = request.POST.get('product_id')
-	if request.is_ajax():
-		print("Ajax Request")
+
 	if product_id is not None:
 		try:
 			product_obj = Product.objects.get(id=product_id)
@@ -28,10 +28,18 @@ def cart_update(request):
 		cart_obj, new_obj = Cart.objects.new_or_get(request)
 		if product_obj in cart_obj.products.all():
 			cart_obj.products.remove(product_obj)
+			added = False
 		else:
 			cart_obj.products.add(product_obj)
+			added = True
 		request.session['cart_items'] = cart_obj.products.count()
-
+		if request.is_ajax():
+			print("Ajax Request")
+			json_data = {
+				"added": added,
+				"removed": not added,
+			}
+			return JsonResponse(json_data)
 	# return redirect(product_obj.get_absolute_url())
 	return redirect('cart:home')
 
